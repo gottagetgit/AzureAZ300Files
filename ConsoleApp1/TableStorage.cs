c089711be76406b90e2d85101f4ad4d3990a8829
Scott Duffy <scottscourse@outlook.com>
2021-01-11 21:30:05 -0500

    Replace documentdb with cosmosdb take 2

--- ConsoleApp1/TableStorage.cs
+++ ConsoleApp1/TableStorage.cs
@@ -1,6 +1,5 @@
-﻿using System;
-using Microsoft.WindowsAzure.Storage;
-using Microsoft.WindowsAzure.Storage.Table;
+﻿using Microsoft.Azure.Cosmos.Table;
+using System;
 
 namespace ConnectToDBApp
 {
